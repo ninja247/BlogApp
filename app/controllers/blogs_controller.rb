b989e4f5026314d9e1d9367ef5f7df047f6f6e73
ninja247 <andrewericoleman@gmail.com>
2017-10-23 09:53:12 -0500

    late homework for creating a blog from scratch

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,15 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @blogs = BlogApp.new
+  end
+
+def edit
+end
+
+
+end
